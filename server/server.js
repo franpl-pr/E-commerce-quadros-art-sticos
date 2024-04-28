6a67105863dd6e9233f2ad45d4841237cb67c4c3
Diogene Gabriel <gabrieldiogene@gmail.com>
2024-04-28 12:08:07 -0300

    Alguma coisa

--- server/server.js
+++ server/server.js
@@ -2,21 +2,6 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql");
 
-//Dados da conexão do banco de dados
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "quadrarts"
-});
-
-app.get('/', (req, res) => {
-    // Realiza a inserção no banco de dados
-    db.query(
-        "INSERT INTO `usuarios` (`ID_usuarios`, `nome`, `email`, `senha`, `confirmarSenha`, `login`, `telefone`, `tipo`, `genero`, `data_nascimento`) VALUES (12 , 'Diogene', 'gabriel@gmail.com, '1234', '1234', 'diogene01', '190834023', 'masculino', '')"
-    );
-});
-
 app.listen(3001, () => {
     console.log("Rodando porta 3001")
 });
