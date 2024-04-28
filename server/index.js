869d7b2a08cfe9887ec369fb0349c6b1c332ae4f
Diogene Gabriel <gabrieldiogene@gmail.com>
2024-04-28 17:33:35 -0300

    Alguma coisa

--- server/index.js
+++ server/index.js
@@ -1,5 +1,25 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "123456",
+    database: "quadrarts"
+})
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/register", (req, res) => {
+    const {name} = req.body;
+    const {cost} = req.body;
+    const {category} = req.body;
+})
+
+console.log(name)
 
 app.listen(3001, () => {
     console.log("Rodando porta 3001");
