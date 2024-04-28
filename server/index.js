cd9561fe112fdfbc525ba5d919c4f621571a103b
Diogene Gabriel <gabrieldiogene@gmail.com>
2024-04-28 16:38:04 -0300

    Alguma coisa

--- server/index.js
+++ server/index.js
@@ -1,19 +1,6 @@
 const express = require("express");
 const app = express();
-const mysql = require("mysql");
-
- const bd = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "quadrarts"
- })
-
-
-app.get('/', (req, res) => {
-    bd.query("INSERT INTO usuarios (ID_usuarios, nome, email, senha, confirmarSenha, login, telefone, tipo) VALUES ('12', 'diogene', 'gabriel@gmail.com', '123456', '123456', 'diogene01', '9873459812', 'Cliente')")
-})
 
 app.listen(3001, () => {
-    console.log("Rodando porta 3001")
+    console.log("Rodando porta 3001");
 });
