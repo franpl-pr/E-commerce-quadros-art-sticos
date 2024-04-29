883a56d80f2734b8ebd66c116b68d7f28b389d9e
Diogene Gabriel <gabrieldiogene@gmail.com>
2024-04-28 23:35:48 -0300

    Acoes comitadas

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.post("/Register", (req, res) => {
             res.send(err)
         }
         if(result.length == 0){
-            let SQL = "INSERT INTO usuarios (ID_usuarios ,nomeCompleto, email, senha, telefone) VALUES ('21' , ?, ?, ?, ?)";
+            let SQL = "INSERT INTO usuarios (nomeCompleto, email, senha, telefone) VALUES ( ?, ?, ?, ?)";
 
             db.query(SQL, [nome, email, senha, telefone], (err, result) => {
                 if(err){
@@ -37,9 +37,13 @@ app.post("/Register", (req, res) => {
             })
         }
         else{
-            res.status(400).send({ error: "Email já cadastrado, tente novamente." });
+            res.send({msg: "Email já cadastrado, tente novamente"})
         }
     })
+})
+
+app.post("/Consultar", (req, res) => {
+
 })
 
 
